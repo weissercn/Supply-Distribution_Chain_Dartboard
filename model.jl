faf3acb56956ec988f0f87f64ac6ed0ae4775711
Connery Noble <connery@qwerti.ca>
2019-11-18 17:54:44 -0500

    Update optimization model

--- model.jl
+++ model.jl
@@ -40,7 +40,7 @@ using JuMP
 using Gurobi
 
 # Loading the historical data file as a dataframe
-df = CSV.read("Dartboard_historical.csv"; header=true);
+df = CSV.read("futurePrediction.csv"; header=true);
 df = df[:,[:FIPS_Code,:Latitude,:Longitude,:Week_Num,:Sales]]
 # Loading DC data
 dc = CSV.read("Dartboard_DCs.csv"; header=true);
@@ -50,9 +50,9 @@ dc = CSV.read("Dartboard_DCs.csv"; header=true);
 ##### Wrangling
 # Extracting data for the period 2012 to 2013 only (i.e weeks 1 to 104).
 # *** This is a different planning horizon from the one described in the case ***
-df_1to104 = df[(df.Week_Num .<= 104),:];
+df_1to104 = df[(df.Week_Num_new .<= 312),:];
 # Extracting data for the last 8 weeks in 2013 - the "peak" demand
-df_97to104 = df_1to104[(df_1to104.Week_Num .> 96),:];
+df_97to104 = df_1to104[(df_1to104.Week_Num .> 304),:];
 
 # Summarizing bt County (while keeping Latitude and Longitude info)
 df_1to104_county = by(df_1to104, [:FIPS_Code, :Latitude, :Longitude], :Sales => sum)
@@ -69,7 +69,7 @@ num_counties = size(df_1to104_county,1);
 demand_1to104 = df_1to104_county[:,:Sales_sum] / 1000;
 demand_97to104 = df_97to104_county[:,:Sales_sum] / 1000;
 
-dc_cap_max = dc[:,:Current_Size] * 5 / 13.5;
+dc_cap_max = dc[:,:Max_Size] * 5 / 13.5;
 
 distances = zeros(num_dc, num_counties) #distance between DCs (rows) and counties, in miles
 for i=1:num_dc, j=1:num_counties
@@ -81,9 +81,9 @@ distances
 
 # TO BE DONE
 
-v #cost per square foot for DC
-trans_cost = 1.55/20; # cost per pallet mile
-fixed_cost_DC = 25000000; # is this the right unit 
+v =  dc[:,:Variable_Cost] * 13.5 / 5; # cost per square foot for DC
+trans_cost = 1.55/20; # cost per pallet mile   $/pallet/mile
+fixed_cost_DC = 25000000; # is this the right unit  $
 
 # End TO BE DONE
 
@@ -98,19 +98,23 @@ model0 = Model(solver=GurobiSolver(MIPGap=0.0001));
 # Note: This decision variable determines whether DC i (row) serves county j (column)
 # NOte: This decision variable is binary, because we assumed that all the demand from one county is served by the same DC
 @variable(model0, x[1:num_dc,1:num_counties], Bin); #if DC i serves county j
-@variable(model0, cc[1:num_dc]); # actually built DC size
+@variable(model0, cc[1:num_dc]); # actually built DC size   sqrfoot
 @variable(model0, z[1:num_dc], Bin); #bool if DC i is built
 
 # Minimize the sum of transportation costs over the 2012-2013 period
 #@objective(model0, Min, trans_cost*sum(demand_1to104[c]*sum(distances[i,c]*x[i,c] for i=1:num_dc) for c=1:num_counties));
-@objective(model0, Min, trans_cost*sum(demand_1to104[c]*sum(distances[i,c]*x[i,c] for i=1:num_dc) for c=1:num_counties) +  fixed_cost_DC *sum(z[i] for i=4:num_dc) + sum(z[i] *v[i] * cc[i]   for i=1:num_dc)   );
+@objective(model0, Min,
+    trans_cost*sum(demand_1to104[c]*sum(distances[i,c]*x[i,c] for i=1:num_dc) for c=1:num_counties) # Transcost
+    + fixed_cost_DC * sum(z[i] for i=4:num_dc)  # Fixed construction cost
+    + sum(z[i] * v[i] * cc[i] for i=4:num_dc)
+    );  # Variable cost
 
 # Top 3 DCs already built
-@constraint(model0, [i in 1:3], z[i] ==1);
+@constraint(model0, [i in 1:3], z[i] == 1);
 @constraint(model0, [i in 1:3], cc[i] == dc_cap_max[i]);
 
 # Capacity can't be bigger than max cap
-@constraint(model0, [i in 1:3], cc[i] <= dc_cap_max[i]);
+@constraint(model0, [i in 4:num_dc], cc[i] <= dc_cap_max[i]*z[i]);
 
 # Each county should use one and only one DC
 @constraint(model0, nosplit[c=1:num_counties], sum(x[i,c] for i=1:num_dc) == 1);
